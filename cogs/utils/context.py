abe748562b1ae3b14399289d2cc8da8dc59861ef
bmintz <bmintz@protonmail.com>
2018-09-23 22:32:45 +0000

    fix ctx.tick

--- cogs/utils/context.py
+++ cogs/utils/context.py
@@ -155,7 +155,7 @@ class Context(commands.Context):
             return confirm
 
     def tick(self, opt, label=None):
-        emoji = '<:greenTick:330090705336664065>' if opt else '<:redTick:330090723011592193>'
+        emoji = '✅' if opt else '<:error:48732221899092889>'
         if label is not None:
             return f'{emoji}: {label}'
         return emoji
