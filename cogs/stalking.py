ed1b36606382da60598b6878931ee21bd8e58692
bmintz <bmintz@protonmail.com>
2019-01-10 08:52:35 +0000

    add stalking cog
    Squashed commit of the following:
    
    commit 91fc00e88ae377e6c77ef9b5023ed81daeaa7aff
    Author: Benjamin Mintz <bmintz@protonmail.com>
    Date:   Thu Jan 10 06:49:48 2019 +0000
    
        wip
    
    commit 150bb171341e948a1cb0a3186166139d47ac4461
    Author: bmintz <bmintz@protonmail.com>
    Date:   Thu Dec 27 21:40:29 2018 +0000
    
        wip
    
    commit d492e63cd4ea77a5be70162361177fb6b101c3dc
    Author: bmintz <bmintz@protonmail.com>
    Date:   Thu Dec 27 21:38:46 2018 +0000
    
        wip
    
    commit 0e1f2eddff3d44b0d6cba9789257eca6101732ca
    Author: bmintz <bmintz@protonmail.com>
    Date:   Thu Dec 27 21:34:49 2018 +0000
    
        wip
    
    commit 5b19489450ef2bd3e8cd01d4bdfbe2aa5c36dd1b
    Author: bmintz <bmintz@protonmail.com>
    Date:   Thu Dec 27 21:31:15 2018 +0000
    
        wip
    
    commit 29418bad5de4e03b3c2a1ac9da24353d081a0de4
    Author: bmintz <bmintz@protonmail.com>
    Date:   Thu Dec 27 21:09:35 2018 +0000
    
        wip
    
    commit f6ab99ea20357f5af8e47b21dd015dd169dc85bd
    Author: bmintz <bmintz@protonmail.com>
    Date:   Thu Dec 27 21:00:23 2018 +0000
    
        wip
    
    commit 770d6deb35a59c2f879486a0a80b85058d0262c9
    Author: bmintz <bmintz@protonmail.com>
    Date:   Wed Dec 26 01:39:25 2018 +0000
    
        wip
    
    commit e1818726c0ec7cc6bd1cc4883d3ae26f60074a0b
    Author: bmintz <bmintz@protonmail.com>
    Date:   Wed Dec 26 00:05:45 2018 +0000
    
        wip
    
    commit cc3df454fdcd2354d311a886228e877b80db5d90
    Author: Benjamin Mintz <bmintz@protonmail.com>
    Date:   Mon Dec 24 23:21:23 2018 +0000
    
        wip
    
    commit 0acc581b7f196d26fab65e6d5f4d20f378d7d9b0
    Author: Benjamin Mintz <bmintz@protonmail.com>
    Date:   Mon Dec 24 04:17:01 2018 +0000
    
        wip
    
    commit 09cfbeb380ca4ea27b7aa71613d714a5d7c05802
    Author: Benjamin Mintz <bmintz@protonmail.com>
    Date:   Mon Dec 24 04:15:00 2018 +0000
    
        wip
    
    commit 9a6ae93fbc46cccb202d8a26334d82f2007e0031
    Author: Benjamin Mintz <bmintz@protonmail.com>
    Date:   Mon Dec 24 03:38:25 2018 +0000
    
        wip
    
    commit eca2fff084f8d1dcbfd5a26b4afe487a79755bbc
    Author: Benjamin Mintz <bmintz@protonmail.com>
    Date:   Mon Dec 24 03:36:35 2018 +0000
    
        wip
    
    commit 54adf43cf1b0bb8c7b350ff333d3f66a3eec741d
    Author: bmintz <bmintz@protonmail.com>
    Date:   Mon Dec 24 01:53:33 2018 +0000
    
        wip
    
    commit a02746170a73eaaae16da6f0b552111a78a4b9c0
    Author: bmintz <bmintz@protonmail.com>
    Date:   Sun Dec 23 08:05:05 2018 +0000
    
        wip
    
    commit 5f4d5ee8b10efdee0eb34dee6359a9f03a2e445d
    Author: bmintz <bmintz@protonmail.com>
    Date:   Sun Dec 23 06:24:45 2018 +0000
    
        wip
    
    commit d8d7fb08fb4c560abebfee97be6690f4aff24db3
    Author: Benjamin Mintz <bmintz@protonmail.com>
    Date:   Sun Dec 23 06:15:29 2018 +0000
    
        wip
    
    commit 841aae2d7e4a56980f2565f470e89b82ad042945
    Author: Benjamin Mintz <bmintz@protonmail.com>
    Date:   Sun Dec 23 06:14:34 2018 +0000
    
        wip
    
    commit d90cdbfcef529883e9c8807ed92f282b21b65af1
    Author: Benjamin Mintz <bmintz@protonmail.com>
    Date:   Sun Dec 23 06:13:04 2018 +0000
    
        wip
    
    commit 9b097b5aec6950bc9c5b271a24f683eee154c110
    Author: Benjamin Mintz <bmintz@protonmail.com>
    Date:   Sun Dec 23 06:05:47 2018 +0000
    
        wip
    
    commit cf3985155b711a556c9dd1009dd05ade4e6e31f6
    Author: Benjamin Mintz <bmintz@protonmail.com>
    Date:   Sun Dec 23 06:01:14 2018 +0000
    
        wip
    
    commit ab259c48c668c8fe039ab5ccd26258a4858e6c5e
    Author: bmintz <bmintz@protonmail.com>
    Date:   Sun Dec 23 00:08:58 2018 +0000
    
        wip
    
    commit 70357250e0278cd8c3367c714e8cb712020fbaa9
    Author: bmintz <bmintz@protonmail.com>
    Date:   Sat Dec 22 23:59:19 2018 +0000
    
        wip

--- cogs/stalking.py
+++ cogs/stalking.py
@@ -1,33 +1,77 @@
+import contextlib
 from dataclasses import dataclass
+import operator
 import time
-from typing import Dict, Set
+from typing import Dict, List, Set
 
 import discord
 from discord.ext import commands
 
-@dataclass
 class StalkedUser:
+	__slots__ = {'user', 'stalkers', 'last_changed'}
+
+	RECENT_STATUS_CHANGE_THRESHOLD = 10  # seconds
+
 	user: discord.User
 	stalkers: Set[discord.User]
 	last_changed: float
 
-	async def notify_stalkers(self, old_status: discord.Status, new_status: discord.Status):
-		for stalker in self.stalkers:
-			with contextlib.suppress(discord.Forbidden):
-				await stalker.send(f"{self.user}'s status just changed from {old_status} to {new_status}")
+	def __init__(self, user, stalkers):
+		self.user = user
+		self.stalkers = stalkers
+		self.last_changed = 0.0
+
+	def not_changed_recently(self):
+		"""return whether the status changed more than N seconds ago"""
+		return self.time_since_last_change() > self.RECENT_STATUS_CHANGE_THRESHOLD
+
+	def time_since_last_change(self):
+		"""return how many seconds elapsed since the last time a presence update was tracked"""
+		return time.monotonic() - self.last_changed
+
+# builtins.id sucks anyway
+id = operator.attrgetter('id')
+ERROR = discord.PartialEmoji(animated=False, name='error', id=487322218989092889)
 
 class Stalking:
-	# values that are "more online"/"more active" come first
-	STATUS_HIERARCHY = dict(map(reversed, enumerate((discord.Status.online, discord.Status.dnd, discord.Status.idle, discord.Status.offline))))
-	STATUS_CHANGED_TRESHOLD = 10
+	# values that are "more online" / "more active" come first
+	STATUS_HIERARCHY = dict(map(reversed, enumerate((
+		discord.Status.online,
+		discord.Status.dnd,
+		discord.Status.idle,
+		discord.Status.offline))))
 
 	def __init__(self, bot):
 		self.bot = bot
-		# map user IDs to their last status change
 		self.stalked: Dict[int, StalkedUser] = {}
+		self.lowest_mutual_guilds: dict[int, discord.Guild] = {}
 
-	async def on_ready(self):
-		await self.bot.change_presence(status=discord.Status.idle)
+		self._register_events()
+
+	async def on_member_update(self, before, after):
+		try:
+			stalked = self.stalked[after.id]
+		except KeyError:
+			return
+
+		# ensure we only handle presence updates once per user
+		if after.guild != self.lowest_mutual_guilds[after.id]:
+			return
+
+		if (
+			self.STATUS_HIERARCHY[after.status] < self.STATUS_HIERARCHY[before.status]
+			and stalked.not_changed_recently()
+		):
+			# the user has become "more online" and stayed that way for a bit
+			await self.notify_stalkers(stalked, before.status, after.status)
+
+		if before.status is not after.status:
+			stalked.last_changed = time.monotonic()
+
+	async def notify_stalkers(self, stalked: StalkedUser, old_status: discord.Status, new_status: discord.Status):
+		for stalker in stalked.stalkers:
+			with contextlib.suppress(discord.Forbidden):
+				await stalker.send(f"{stalked.user}'s status just changed from {old_status} to {new_status}")
 
 	@commands.command()
 	@commands.is_owner()
@@ -36,43 +80,72 @@ class Stalking:
 		try:
 			stalked = self.stalked[user.id]
 		except KeyError:
-			self.stalked[user.id] = StalkedUser(user=user, stalkers={ctx.author}, last_changed=time.monotonic())
+			self.stalked[user.id] = StalkedUser(user=user, stalkers={ctx.author})
 		else:
 			stalked.stalkers.add(ctx.author)
 
-		await ctx.send(f"""✅ I will DM you when {user}'s status "increases".""")
+		self.update_lowest_mutual_guild(user)
+
+		await ctx.send(f"""✅ I will DM you when {user}'s status "increases".""", delete_after=5.0)
 
 	@commands.command()
 	async def unstalk(self, ctx, *, user: discord.User):
 		"""stops stalking someone"""
-		self.stalked.pop(user.id, None)
+
+		error_not_stalking = f"{ERROR} You're not stalking this user."
+
+		try:
+			stalkers = self.stalked[user.id].stalkers
+		except KeyError:
+			# nobody is stalking this user
+			await ctx.send(error_not_stalking)
+			return
+
+		try:
+			stalkers.remove(ctx.author)
+		except KeyError:
+			await ctx.send(error_not_stalking)
+			return
+
+		if not stalkers:
+			del self.stalked[user.id]
+			self.lowest_mutual_guilds.pop(user.id, None)
+
 		await ctx.message.add_reaction('✅')
 
 	@stalk.error
 	async def stalk_error(self, ctx, error):
 		if isinstance(error, commands.UserInputError):
-			return await ctx.send(error)
-		if isinstance(error, commands.NotOwner):
+			await ctx.send(error)
+		elif isinstance(error, commands.NotOwner):
 			with contextlib.suppress(discord.Forbidden):
-				await ctx.message.add_reaction(discord.PartialEmoji(animated=False, name='error', id=487322218989092889))
+				await ctx.message.add_reaction(ERROR)
+		else:
+			raise
 
-		raise
+	### Presence event deduplication
 
-	async def on_member_update(self, before, after):
-		try:
-			stalked = self.stalked[after.id]
-		except KeyError:
+	def update_lowest_mutual_guild(self, member, _id=id):
+		if member.id not in self.stalked:
 			return
 
-		if (
-			self.STATUS_HIERARCHY[after.status] < self.STATUS_HIERARCHY[before.status]
-			and time.monotonic() - stalked.last_changed > self.STATUS_CHANGED_TRESHOLD
-		):
-			# the user has been "more online" for a little while
-			await stalked.notify_stalkers()
+		self.lowest_mutual_guilds[member.id] = min(
+			(guild for guild in self.bot.guilds if guild.get_member(member.id)),
+			key=_id)
 
-		if before.status is not after.status:
-			stalked.last_changed = time.monotonic()
+	async def _on_member_join_leave(self, member):
+		self.update_lowest_mutual_guild(member)
+
+	async def _on_guild_join_remove(self, guild):
+		for member in guild.members:
+			self.update_lowest_mutual_guild(member)
+
+	def _register_events(self):
+		for event in 'on_member_join', 'on_member_leave':
+			self.bot.add_listener(self._on_member_join_leave, event)
+
+		for event in 'on_guild_join', 'on_guild_remove':
+			self.bot.add_listener(self._on_guild_join_remove, event)
 
 def setup(bot):
 	bot.add_cog(Stalking(bot))
